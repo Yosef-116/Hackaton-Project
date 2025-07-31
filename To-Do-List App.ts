924c106d6442952cd2be765f89cfb5dd7ee199f1
Yosef-116 <josephhassen41@gmail.com>
2025-08-01 00:39:01 +0300

    added while loop and if condition

--- To-Do-List App.ts
+++ To-Do-List App.ts
@@ -1,17 +1,46 @@
+import PromptSync = require('prompt-sync')
 
-const choice = Number(prompt('Enter your choice: '));
+const prompt = PromptSync()
+
+console.log('Welcome To Task Manager!!!');
+
+let check = true;
+
+while(check){
+
+    console.log(`
+----------------------------
+What would you like to do?
+1. Add Task
+2. View Tasks
+3. Mark Task as Completed
+4. Delete Task
+5. Exit
+----------------------------`);
+const choice = Number(prompt('Enter your choice(1, 2, 3, 4, 5): '));
 // Adding task  
 if(choice === 1){
         const choice1 = prompt('Enter the task description: ')
         tasks.push({task: choice1, completed: false});
              console.log('The task has been added!');
+            }
   //viewing the tasks
-      else if (choice === 2) {
+    else if (choice === 2) {
         console.log('Your tasks:');
-        } else {
-            tasks.forEach(task => {
+        tasks.forEach((task,index) => {
                 const status = task.completed ? '(done)' : '(not done)';
-                console.log('- ' + task.task + ' ' + status);
+                console.log(`${index + 1}` + task.task + ' ' + status);
             });
         }
+    else if(choice === 3){
+
+    }else if(choice === 4){
+
+    }else if(choice === 5){
+
+    }else{
+        console.log('You have entered an invalid choice');
     }
+}
+    
+
