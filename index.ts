eb56fcbe59e773b386fb616144f0ccdb76dcd802
Barkotfrew <141666169+Barkotfrew@users.noreply.github.com>
2025-08-01 09:59:46 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -20,8 +20,16 @@ What would you like to do?
 
     const choice = Number(prompt('Enter your choice(1, 2, 3, 4, 5): '));
     if(choice === 1){
-
+        const choice1 = prompt('Enter the task description: ')
+        tasks.push({task: choice1, completed: false});
+             console.log('The task has been added!');
+    }
     }else if(choice === 2){
+        console.log('---Your tasks---');
+        tasks.forEach((task,index) => {
+         console.log(${index + 1} ${task.task});
+})
+console.log(tasks);
 
     }else if(choice === 3){
 
