a5929a92b90632a1f2afd4942893f5f7a724a99e
Abigiya <abigiyatesema11@gmail.com>
2025-08-01 03:05:04 +0300

    Added Add Task and Mark Task as Completed features

--- index.ts
+++ index.ts
@@ -1,4 +1,40 @@
-  const choice = Number(prompt('Enter your choice: '));
+import PromptSync = require('prompt-sync')
+
+const prompt = PromptSync()
+
+console.log('Welcome To Task Manager!!!');
+
+let check = true;
+
+while(check){
+
+    console.log(`
+----------------------------
+What would you like to do?
+1. Add Task
+2. View Tasks
+3. Mark Task as Completed
+4. Delete Task
+5. Exit
+----------------------------`);
+
+    const choice = Number(prompt('Enter your choice(1, 2, 3, 4, 5): '));
+    if(choice === 1){
+
+    }else if(choice === 2){
+
+    }else if(choice === 3){
+
+    }else if(choice === 4){
+
+    }else if(choice === 5){
+
+    }else{
+        console.log('You have entered an invalid choice');
+    }
+}
+
+ const choice = Number(prompt('Enter your choice: '));
     if(choice === 1){
         const choice1 = prompt('Enter the task description: ')
         tasks.push({task: choice1, completed: false});
@@ -17,3 +53,6 @@
         }else{
             console.log("Invalid task number");   
         }}
+
+
+
