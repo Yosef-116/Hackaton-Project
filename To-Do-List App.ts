80db65a3e0ded071d7b54b35858478ac6bf00ba1
Yosef-116 <josephhassen41@gmail.com>
2025-08-02 18:26:31 +0300

    Final commit

--- To-Do-List App.ts
+++ To-Do-List App.ts
@@ -1,8 +1,14 @@
-import PromptSync = require('prompt-sync')
-
+// import PromptSync = require('prompt-sync')
+import PromptSync from 'prompt-sync'
 const prompt = PromptSync()
 
 console.log('Welcome To Task Manager!!!');
+
+interface Task { 
+    task : string;
+    isCompleted : boolean;
+} 
+let tasks: Task[] = [];
 
 let check = true;
 
@@ -21,23 +27,39 @@ const choice = Number(prompt('Enter your choice(1, 2, 3, 4, 5): '));
 // Adding task  
 if(choice === 1){
         const choice1 = prompt('Enter the task description: ')
-        tasks.push({task: choice1, completed: false});
-             console.log('The task has been added!');
+        tasks.push({task: choice1, isCompleted: false});
+        console.log('The task has been added!');
             }
   //viewing the tasks
     else if (choice === 2) {
         console.log('Your tasks:');
         tasks.forEach((task,index) => {
-                const status = task.completed ? '(done)' : '(not done)';
-                console.log(`${index + 1}` + task.task + ' ' + status);
+            const status = task.isCompleted ? '(done)' : '(not done)';
+            console.log(`${index + 1}.` + task.task + ' ' + status);
             });
         }
+    // Mark as completed
     else if(choice === 3){
-
+        const index = Number(prompt("Enter task number to mark as completed: ")) -1;
+        if(tasks[index]){
+            tasks[index].isCompleted = true;
+            console.log(tasks);
+            console.log("Task marked as completed");
+        }else{
+            console.log("Invalid task number");   
+        }
+        // Delete task
     }else if(choice === 4){
-
+        const index = Number(prompt("Enter task number to delete: ")) - 1;
+        if(tasks[index]){
+            tasks.splice(index, 1);
+            console.log('The chosen task has been deleted')
+        }else{
+        console.log('You have entered an invalid choice');}
+        // Exit
     }else if(choice === 5){
-
+        console.log('---Task Manager is Closed---');
+        check = false;
     }else{
         console.log('You have entered an invalid choice');
     }
